9d74afc46147a4c1d627bee0f3f333493f162d87
Sit Lo <141769635+Official3Lo@users.noreply.github.com>
2023-11-02 15:57:52 -0700

    Webscrapper.java pom.xml jsoup-1.16.2.jar
    Submission of the webscrapper program [10-31-23]

---
+++ Webscrapper.java
@@ -0,0 +1,33 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+public class Webscrapper {
+    public static void main(String[] args) {
+
+        System.out.printf("Welcome to web-scraping program for FCC");
+
+        try {
+            // Specify the URL to scrape
+            String url = "https://www.fresnocitycollege.edu/";
+
+            // Fetch and parse the HTML document from the URL
+            Document document = Jsoup.connect(url).get();
+
+            // Extract all the links and their titles from the document
+            Elements links = document.select("a[href]");
+            for (Element link : links) {
+                // Extract the link and the title
+                String linkHref = link.attr("abs:href");
+                String linkText = link.text();
+                System.out.println("Link: " + linkHref + ", Text: " + linkText);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
